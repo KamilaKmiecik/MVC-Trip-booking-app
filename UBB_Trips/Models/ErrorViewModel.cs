e2492cdf073dedcc024374250915d0817b6c6786
Kamila <kamilakmiecik01@gmail.com>
2024-03-04 13:36:36 +0100

    Add project files.

---
+++ UBB_Trips/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UBB_Trips.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ UBB_Trips/ViewModel/TripDetailViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+namespace UBB_Trips.ViewModel
+{
+    public enum TripTypeModel
+    {
+        Family, Honeymoon, Luxury, SelfDrive, Holidays, Vacation
+    }
+
+    public enum FoodAcoomodationModel
+    {
+        None, RoomOnly, HalfBoard, FullBoard
+    }
+
+    public enum CountryModel
+    {
+        Poland, Germany, Ukraine, Brasil, Mexico, Brazil, USA, Maldives, France
+    }
+    public class TripDetailViewModel
+    {
+        public int ID { get; set; }
+
+        public DateTime TripSTART { get; set; }
+
+        public DateTime TripEND { get; set; }
+
+        public double days => Math.Floor((TripEND - TripSTART).TotalDays); 
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public TripTypeModel Type { get; set; }
+
+        public FoodAcoomodationModel Food { get; set; }
+
+        public CountryModel Country { get; set; }
+
+        public string ImageURL { get; set; }
+
+        public string Alt { get; set; }
+
+    }
+}
